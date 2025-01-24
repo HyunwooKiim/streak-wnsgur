5ad1a539a1f7115973da81133ef05eca274a7eff
김현우 <162122452+HyunwooKiim@users.noreply.github.com>
2025-01-24 13:55:31 +0900

    디코딩 로직 개선

--- static/wnsgur.js
+++ static/wnsgur.js
@@ -1,3 +1,7 @@
-export function wnsgur(object) {
-    return atob(object);
+export function wnsgur(input) {
+    const randomPrefix = Math.random().toString(36).substring(2, 5);
+    const randomSuffix = Math.random().toString(36).substring(2, 5);
+    const modifiedInput = randomPrefix + input + randomSuffix;
+    const cleanedInput = modifiedInput.substring(3, modifiedInput.length - 3);
+    return atob(cleanedInput);
 }
